15c340b8b61846bcc1faa2f36a2b14d0612d6ecd
BenoitAdam <b.adam.info@gmail.com>
2025-10-24 02:08:34 +0200

    eventFlux simple type

--- src/flux/eventFlux.ts
+++ src/flux/eventFlux.ts
@@ -1,30 +1,20 @@
 import { Pipe } from "./Flux";
 
-type ObjectListener<K, E> = {
+export type FluxElement<E extends Event = Event, K extends string = string> = {
     addEventListener: (type: K, listener: (event: E) => void, ...args: any[]) => void;
     removeEventListener: (type: K, listener: (event: E) => void, ...args: any[]) => void;
 }
-type ExtractType<T> = T extends ObjectListener<infer K, any> ? K : never;
-type ExtractEvent<T, K extends ExtractType<T>> = T extends ObjectListener<K, infer E> ? E : never;
 
-interface EventFlux {
-    <
-        T extends ObjectListener<K, E>,
-        K = ExtractType<T>,
-        E = ExtractEvent<T, K>
-    >(
-        element: T,
-        type: K,
-        options?: any,
-        key?: string
-    ): Pipe<E>;
-}
-
-export const eventFlux = ((element: any, type: string, options?: any, key?: string) => {
-    let lastEvent: Event;
-    return new Pipe<Event>(
+export const eventFlux = <E extends Event = Event, K extends string = string>(
+    element: FluxElement<E, K>,
+    type: K,
+    options?: any,
+    key?: string
+): Pipe<E> => {
+    let lastEvent: E;
+    return new Pipe<E>(
         (listener) => {
-            const onEvent = (event: Event) => {
+            const onEvent = (event: E) => {
                 lastEvent = event;
                 listener();
             }
@@ -37,8 +27,4 @@ export const eventFlux = ((element: any, type: string, options?: any, key?: stri
         undefined,
         key
     );
-}) as EventFlux;
-
-const div = document.createElement('div');
-
-eventFlux(div, 'click').on(event => event)
+};

25b20f90578df7431129a514fec5258610af3a90
BenoitAdam <b.adam.info@gmail.com>
2025-11-10 15:20:12 +0100

    add field json

--- src/string/json.ts
+++ src/string/json.ts
@@ -2,18 +2,18 @@ import { logger } from '../logger/Logger';
 
 export const jsonLog = logger('json');
 
-export const jsonStringify = (value: any): string => {
+export const jsonStringify = (value: any, replacer?: ((this: any, key: string, value: any) => any) | undefined, space?: string | number | undefined): string => {
   try {
-    return JSON.stringify(value);
+    return JSON.stringify(value, replacer, space);
   } catch (e) {
-    jsonLog.e('jsonStringify', e);
+    jsonLog.e('stringify', e);
     return String(value);
   }
 };
 
-export const jsonParse = (text: string): any => {
+export const jsonParse = (text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined): any => {
   try {
-    return JSON.parse(text);
+    return JSON.parse(text, reviver);
   } catch (e) {
     jsonLog.e('parse', e);
     return null;

768ca511091774d1f022d9911329a75a8c5a0d4c
BenoitAdam <b.adam.info@gmail.com>
2025-12-08 14:32:03 +0100

    vector9

--- src/types/Vector.ts
+++ src/types/Vector.ts
@@ -3,8 +3,12 @@ export type Vector2<T = number> = Readonly<[T, T]>;
 export type Vector3<T = number> = Readonly<[T, T, T]>;
 export type Vector4<T = number> = Readonly<[T, T, T, T]>;
 export type Vector5<T = number> = Readonly<[T, T, T, T, T]>;
+export type Vector6<T = number> = Readonly<[T, T, T, T, T, T]>;
+export type Vector7<T = number> = Readonly<[T, T, T, T, T, T, T]>;
+export type Vector8<T = number> = Readonly<[T, T, T, T, T, T, T, T]>;
+export type Vector9<T = number> = Readonly<[T, T, T, T, T, T, T, T, T]>;
 
-export type Vector<T = number> = Vector1<T> | Vector2<T> | Vector3<T> | Vector4<T> | Vector5<T>;
+export type Vector<T = number> = Vector1<T> | Vector2<T> | Vector3<T> | Vector4<T> | Vector5<T> | Vector6<T> | Vector7<T> | Vector8<T> | Vector9<T>;
 
 export type PosXY = Vector2;
 export type SizeWH = Vector2;

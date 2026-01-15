cf0b0c127a8e8dd0f7b6de48318c98257adffc54
Benoit ADAM <b.adam.info@gmail.com>
2026-01-15 18:03:41 +0100

    minifyUuid

--- src/string/uuid.ts
+++ src/string/uuid.ts
@@ -1,3 +1,4 @@
+import { isUuid } from "fluxio/check";
 import { randHex } from "./randString";
 
 /**
@@ -11,9 +12,13 @@ export const uuid = (): string => {
   return `${randHex(8)}-${randHex(4)}-4${randHex(3)}-8${randHex(3)}-${randHex(12)}`;
 };
 
-export const minifyUuid = (uuid: string) => uuid.replace(/-/g, '');
+export const minifyUuid = (uuid: string) => {
+    if (!isUuid(uuid)) return '';
+    return uuid.replace(/-/g, '');
+}
 
 export const prettifyUuid = (uuid: string) => {
   const h = minifyUuid(uuid);
+  if (!h) return '';
   return `${h.slice(0, 8)}-${h.slice(8, 12)}-${h.slice(12, 16)}-${h.slice(16, 20)}-${h.slice(20)}`;
 };

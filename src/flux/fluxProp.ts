296dbf55d88da93c7b35fbefd2f3188a7143209c
Benoit ADAM <b.adam.info@gmail.com>
2026-01-22 15:54:41 +0100

    fluxProp

--- src/flux/fluxProp.ts
+++ src/flux/fluxProp.ts
@@ -7,10 +7,11 @@ export const fluxProp = <T extends {}, P extends keyof T>(source: Flux<T>, prop:
     (pipe) => {
       const obj = source.get();
       const value = isItem(obj) ? obj[prop] : undefined;
-      pipe.set(value as T[P]);
+      pipe.set(value as T[P], true);
     },
     (value) => {
       const item = source.get();
+      if (isItem(item) && item[prop] === value) return;
       source.set({ ...item, [prop]: value } as T);
     }
   );

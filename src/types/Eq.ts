eac09585bfd179458b14e7b0eda6e95613b3a6e6
Benoit Adam <b.adam.info@gmail.com>
2026-02-04 16:56:06 +0000

    format

--- src/types/Eq.ts
+++ src/types/Eq.ts
@@ -1,8 +1,12 @@
 export type Eq<A, B> =
-  (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2)
-    ? true
-    : false;
+  (<T>() => T extends A ? 1 : 2) extends <T>() => T extends B ? 1 : 2 ? true : false;
 
-export type CheckEq<A, B> = Eq<A, B> extends true
-  ? true
-  : ['Types differ', 'Only in A:', Exclude<keyof A, keyof B>, 'Only in B:', Exclude<keyof B, keyof A>];
+export type CheckEq<A, B> =
+  Eq<A, B> extends true ? true
+  : [
+      'Types differ',
+      'Only in A:',
+      Exclude<keyof A, keyof B>,
+      'Only in B:',
+      Exclude<keyof B, keyof A>,
+    ];

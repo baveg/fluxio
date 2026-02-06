c6d3e3b1b24be3ea6081af13e6392d88d294a358
Benoit Adam <b.adam.info@gmail.com>
2026-02-06 13:42:42 +0000

    singletons

--- src/object/singleton.ts
+++ src/object/singleton.ts
@@ -1,25 +1,33 @@
+import { logger } from '../logger';
 import type { Dictionary } from '../types/Dictionary';
-import { glb } from 'fluxio/glb';
 
-const fluxio: Dictionary<any> = glb.fluxio || (glb.fluxio = {});
-const singletons: Dictionary<any> = fluxio.singletons || (fluxio.singletons = {});
+const log = logger('singleton');
+export const singletons: Dictionary<any> = {};
 
 export const singleton = <T>(clazz: { prototype: T, name: string }): {
     (): T;
     set(instance: T): void;
     reset(): void;
 } => {
-  const name = clazz.name;
+  const name = String(clazz.name);
 
-  const get = () => singletons[name] || (
-    singletons[name] = new (clazz as unknown as new () => T)()
-  );
+  const get = () => {
+    let singleton = singletons[name];
+    if (!singleton) {
+      log.d('create', name, singletons);
+      singleton = new (clazz as unknown as new () => T)()
+      singletons[name] = singleton;
+    }
+    return singleton;
+  }
   
   get.set = (instance: T) => {
+    log.d('set', name, instance);
     singletons[name] = instance;
   };
 
   get.reset = () => {
+    log.d('reset', name);
     singletons[name] = undefined;
   };
 

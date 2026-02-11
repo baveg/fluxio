6711f093e4052385da03dd9a144ce0e1e3bac1d9
Benoit Adam <b.adam.info@gmail.com>
2026-02-11 09:45:48 +0000

    add Accordion

---
+++ src/ui/components/Accordion.tsx
@@ -0,0 +1,43 @@
+import { Css } from '../../html/css';
+import { comp, Comp } from '../utils/comp';
+import { useState } from 'preact/hooks';
+import { DivProps } from './types';
+
+const c = Css('Accordion', {
+  '': {
+    h: 200,
+    w: 200,
+    wh: 100,
+    col: ['stretch', 'around'],
+  },
+  // '-open': {
+  // },
+  // '-close': {
+  // },
+});
+
+export interface AccordionProps extends DivProps {
+  headerProps?: DivProps;
+  contentProps?: DivProps;
+  header: Comp;
+  children: Comp;
+}
+
+export const Accordion = ({ header, children, headerProps, contentProps, ...props }: AccordionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handle = () => setIsOpen(prev => !prev);
+
+  return (
+    <div {...props} {...c('', isOpen ? '-open' : '-close', props)}>
+      <div {...headerProps} {...c('Header', headerProps)} onClick={handle}>
+        {comp(header)}
+      </div>
+      {isOpen && (
+        <div {...contentProps} {...c('Content', contentProps)}>
+          {comp(children)}
+        </div>
+      )}
+    </div>
+  )
+}

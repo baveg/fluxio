43496881a6cb836fbb12061e34bf59a0fa0b7db6
Benoit ADAM <b.adam.info@gmail.com>
2026-02-24 15:12:43 +0100

    cleanSearch

--- src/string/isSearched.ts
+++ src/string/isSearched.ts
@@ -1,16 +1,28 @@
 import { removeAccents } from './removeAccents';
 import { lower } from './lower';
+import { Item } from 'fluxio/types';
+import { humanize } from './humanize';
+
+export const cleanSearch = (search: string | null | undefined) => (
+  search ? lower(removeAccents(search)).trim() : ''
+)
+
+export const getSearchTags = (item: Item|null|undefined) => item ? cleanSearch(humanize(Object.values(item))) : '';
 
 export const isSearched = (
   source: string | null | undefined,
-  search: string | null | undefined
+  search: string | null | undefined,
+  clean = true,
 ) => {
   if (!search) return true;
   if (!source) return false;
-  const sourceCleaned = lower(removeAccents(source)).trim();
-  const searchTags = lower(removeAccents(search)).trim().split(' ');
+  if (clean) {
+    source = cleanSearch(source);
+    search = cleanSearch(search);
+  }
+  const searchTags = search.split(' ');
   for (const tag of searchTags) {
-    if (sourceCleaned.indexOf(tag) === -1) return false;
+    if (source.indexOf(tag) === -1) return false;
   }
   return true;
 };

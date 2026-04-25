525e34b31be6b166f0be27ec9a6986d5d8dd7ef5
Benoit ADAM <b.adam.info@gmail.com>
2026-04-25 12:02:11 +0200

    ElProps

--- src/ui/components/types.tsx
+++ src/ui/components/types.tsx
@@ -2,8 +2,8 @@ import type { JSX } from 'preact';
 
 export type GetProps<T> = Omit<T, 'style'> & { style?: any };
 
-export type Props = {
+export type ElProps = {
   [K in keyof JSX.IntrinsicElements]: GetProps<JSX.IntrinsicElements[K]>;
 };
 
-export type DivProps = Props['div'];
+export type DivProps = ElProps['div'];

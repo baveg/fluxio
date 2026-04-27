e6baf014eca855c6bf11d96dd7a8e124ba614a57
Benoit ADAM <b.adam.info@gmail.com>
2026-04-27 14:53:27 +0200

    snakeCase allow $

--- src/string/cases.ts
+++ src/string/cases.ts
@@ -7,7 +7,7 @@ export const pascalToKebabCase = (v: string): string =>
 export const getWords = (v: string) =>
   v
     .replace(/[a-z0-9][A-Z]/g, (s) => s[0] + ' ' + (s[1] || ''))
-    .replace(/[^a-z0-9A-Z]+/g, () => ' ')
+    .replace(/[^a-z0-9A-Z$]+/g, () => ' ')
     .trim()
     .toLowerCase()
     .split(' ');

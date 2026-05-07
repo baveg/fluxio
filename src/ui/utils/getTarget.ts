4498be2aa9313f519d7a2600b34808e113d72dd9
Benoit ADAM <b.adam.info@gmail.com>
2026-05-07 16:08:28 +0200

    getTarget

--- src/ui/utils/getTarget.ts
+++ src/ui/utils/getTarget.ts
@@ -1,3 +1,6 @@
-export const getTarget = (e: HTMLInputElement | Event): HTMLElement|undefined => (
-    e instanceof HTMLElement ? e : (e.target ?? e.currentTarget) as HTMLElement
-);
+export const getTarget = (eventOrTarget: Event | HTMLElement): HTMLElement|undefined => {
+    if (eventOrTarget instanceof HTMLElement) return eventOrTarget;
+    const el = eventOrTarget.currentTarget ?? eventOrTarget.target;
+    if (el instanceof HTMLElement) return el;
+    return undefined;
+}

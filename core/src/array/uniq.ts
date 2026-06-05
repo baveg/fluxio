f5588e7b3b91fab1a4114d8ca8dde702efb0ebc9
Benoit ADAM <b.adam.info@gmail.com>
2026-06-05 16:35:19 +0200

    uniq with map

--- core/src/array/uniq.ts
+++ core/src/array/uniq.ts
@@ -1,11 +1,29 @@
 import type { Dictionary } from '../types/Dictionary';
 import { jsonStringify } from '../string/json';
 import { getValues } from '../object/getValues';
+import { isString } from '../check/isString';
+import { isNumber } from '../check/isNumber';
 
-export const uniq = <T>(a: T[], isJson = true): T[] => {
+interface Uniq {
+  <T>(a: T[]): T[];
+  <T, U>(a: T[], map: (v: T) => U): U[];
+}
+
+/**
+ * Returns an array with unique values. Optionally applies a mapping function before deduplication.
+ *
+ * @example
+ * uniq([1, 2, 2, 3, 1]) // [1, 2, 3]
+ * uniq(['a', 'b', 'a']) // ['a', 'b']
+ * uniq([{id: 1}, {id: 2}, {id: 1}]) // [{id: 1}, {id: 2}]
+ * uniq([{id: 1, name: 'a'}, {id: 2, name: 'b'}, {id: 1, name: 'c'}], v => v.id) // [1, 2]
+ */
+export const uniq: Uniq = <T, U>(a: T[], map?: (v: T) => U): U[] => {
   const o: Dictionary<any> = {};
-  for (const v of a) {
-    o[(isJson ? jsonStringify(v) : null) || String(v)] = v;
+  let v: T|U;
+  for (v of a) {
+    if (map) v = map(v);
+    o[isString(v) ? v : isNumber(v) ? String(v) : jsonStringify(v)] = v;
   }
   return getValues(o);
 };

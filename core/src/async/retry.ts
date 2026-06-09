cca00a9ea222b2081188be1d05386f2c53db8c31
Benoit ADAM <b.adam.info@gmail.com>
2026-06-09 15:34:27 +0200

    retry with delayMs

--- core/src/async/retry.ts
+++ core/src/async/retry.ts
@@ -1,10 +1,16 @@
-export const retry = async <T>(createPromise: () => Promise<T>, retry = 2): Promise<T> => {
+import { sleep } from "./sleep";
+
+export const retry = async <T>(createPromise: () => Promise<T>, retry = 2, delayMs = 100): Promise<T> => {
   let error: any;
   for (let i = 0; i < retry; i++) {
     try {
       return await createPromise();
     } catch (e) {
       error = e;
+      if (i < retry - 1) {
+        await sleep(delayMs);
+        delayMs *= 2;
+      }
     }
   }
   throw error;

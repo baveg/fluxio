1faa07a51b02939063f17cf7ab30c0f93516fac7
Benoit ADAM <b.adam.info@gmail.com>
2026-06-25 14:13:22 +0200

    add jsonStringify depth

--- core/src/string/json.ts
+++ core/src/string/json.ts
@@ -1,16 +1,29 @@
+import { isArray, isObject } from '../check';
 import { logger } from '../logger/Logger';
+import { by } from '../object/by';
 
-export const jsonLog = logger('json');
+export const log = logger('json');
 
 export const jsonStringify = (
   value: any,
   replacer?: ((this: any, key: string, value: any) => any) | undefined,
-  space?: string | number | undefined
+  space?: string | number | undefined,
+  depth: number = 3,
 ): string => {
   try {
     return JSON.stringify(value, replacer, space);
   } catch (e) {
-    jsonLog.e('stringify', e);
+    log.w('stringify error', value, e);
+    if (isArray(value)) {
+      if (!depth) return '[]';
+      const copy = value.map(v => jsonParse(jsonStringify(v, replacer, space, depth - 1)));
+      return jsonStringify(copy);
+    }
+    if (isObject(value)) {
+      if (!depth) return '{}';
+      const copy = by(value, null, v => jsonParse(jsonStringify(v, replacer, space, depth - 1)));
+      return jsonStringify(copy);
+    }
     return String(value);
   }
 };
@@ -22,7 +35,7 @@ export const jsonParse = (
   try {
     return text ? JSON.parse(text, reviver) : null;
   } catch (e) {
-    jsonLog.e('parse', e);
+    log.e('parse', e);
     return null;
   }
 };

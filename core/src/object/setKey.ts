337913162d443dabf69dbea34e2d15aa82616e6e
Benoit ADAM <b.adam.info@gmail.com>
2026-07-02 03:55:34 +0200

    setKey

--- core/src/object/setKey.ts
+++ core/src/object/setKey.ts
@@ -1,4 +1,11 @@
-export const setKey = <T, K extends keyof T>(record: T, key: K, value: T[K]): T => {
-  record[key] = value;
+/**
+ * Sets `record[key]` to `value`, or deletes the key when `value` is `undefined`. Mutates and returns `record`.
+ * @example
+ * setKey({ a: 1, b: 2 }, 'a', 5); // { a: 5, b: 2 }
+ * setKey({ a: 1, b: 2 }, 'a', undefined); // { b: 2 }
+ */
+export const setKey = <T, K extends keyof T>(record: T, key: K, value?: T[K]|undefined): T => {
+  if (value === undefined) delete record[key];
+  else record[key] = value;
   return record;
 };

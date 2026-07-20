0b51b9e44b5b1c1fa6dd02293ba427f0109927cf
Benoit ADAM <b.adam.info@gmail.com>
2026-07-20 13:20:01 +0200

    withRetry

--- core/src/async/withRetry.ts
+++ core/src/async/withRetry.ts
@@ -1,19 +1,19 @@
-import { SECOND } from '../date/date';
 import { sleep } from './sleep';
 
 /**
  * Retries a promise with exponential backoff delay
- *
- * @param factory   Async function that creates the promise to retry
- * @param retry - Number of retry attempts (default: 3)
- * @param delayMs - Initial delay in ms, doubles after each retry (default: 500ms → 2s → 8s)
+ * @param factory Async function that creates the promise to retry
+ * @param retry Number of retry attempts (default: 10)
+ * @param delayMs Delay in ms (default: 1s)
+ * @param firstMs Initial delay in ms (default: 200ms)
  * @returns The result of the promise if successful
  * @throws The last error if all retries fail
  */
 export const withRetry = <F extends (...args: any[]) => Promise<any>>(
   factory: F,
-  retry: number = 3,
-  delayMs = SECOND / 2
+  retry: number = 10,
+  delayMs = 1000,
+  firstMs = 200,
 ): F => {
   return (async (...args: any[]) => {
     let error: any;
@@ -22,8 +22,7 @@ export const withRetry = <F extends (...args: any[]) => Promise<any>>(
         return await factory(...args);
       } catch (e) {
         error = e;
-        await sleep(delayMs);
-        delayMs *= 4;
+        await sleep(i === 0 ? firstMs : delayMs);
       }
     }
     throw error;

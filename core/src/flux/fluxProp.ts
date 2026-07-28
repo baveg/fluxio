5e2fd36f450fa8f0c022d8c83babca84c4a3fd54
Benoit ADAM <b.adam.info@gmail.com>
2026-07-28 22:56:50 +0200

    add fluxProp clean

--- core/src/flux/fluxProp.ts
+++ core/src/flux/fluxProp.ts
@@ -3,16 +3,19 @@ import { isItem } from '../check/isItem';
 
 export const fluxProp = <T extends {}, P extends keyof T>(
   source: Flux<T>,
-  prop: P
+  prop: P,
+  clean?: (v: T[P]) => T[P],
 ): Pipe<T[P], T[P]> => {
   return new Pipe<T[P], T[P]>(
     source as any,
     (pipe) => {
       const obj = source.get();
-      const value = isItem(obj) ? obj[prop] : undefined;
+      let value = isItem(obj) ? obj[prop] : undefined;
+      if (clean) value = clean(value as T[P]);
       pipe.set(value as T[P], true);
     },
     (value) => {
+      if (clean) value = clean(value);
       const item = source.get();
       if (isItem(item) && item[prop] === value) return;
       source.set({ ...item, [prop]: value } as T);
